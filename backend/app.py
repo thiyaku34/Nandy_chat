b1f8d81542f058277012120b4f7f54c65a96e1e8
thiyaku <thiyakueali@gmail.com>
2026-03-18 22:08:32 +0530

    added flask-bcrypt

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_socketio import SocketIO, emit
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt   
 
 bcrypt = Bcrypt()
 bcrypt.init_app(app)
